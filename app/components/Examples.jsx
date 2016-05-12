1275cfb72b32817160cc9388785e1706aad4a249
dracula57 <robin.vitre@gmail.com>
2016-05-12 10:45:46 +0200

    add foundation and jq

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var { Link } = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p> Welcome </p>
+      <h1 className="text-center">Examples</h1>
+      <p> Few examples here </p>
+      <ol>
+        <li>
+          <Link to="/?location=Metz"> Metz, FR</Link>
+        </li>
+        <li>
+          <Link to="/?location=Paris"> Paris, FR</Link>
+        </li>
+      </ol>
     </div>
   );
 }
