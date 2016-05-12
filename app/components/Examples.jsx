0704a1f4bc75959ec25e65c456cfe7541fbd43cd
dracula57 <robin.vitre@gmail.com>
2016-05-12 09:31:14 +0200

    rename example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,8 +2,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h3>Examples</h3>
-    <p> Welcome </p>
+    <div>
+      <h3>Examples</h3>
+      <p> Welcome </p>
+    </div>
   );
 }
 
